97417a62e5b1a46d35b743d4042544c5d78ab2bf
steve <ionc635@naver.com>
2023-01-29 12:28:56 +0900

    solve: 2주차_개인

--- steven/main.go
+++ steven/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"APS/steven/greedy"
+	"APS/steven/math"
 	"APS/steven/string"
 )
 
@@ -9,4 +10,5 @@ func main() {
 	string.LengthOfLastWord("hello world")
 	greedy.MaxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
 	greedy.MaxProfit([]int{7, 1, 5, 3, 6, 4})
+	math.IsPalindrome(121)
 }
