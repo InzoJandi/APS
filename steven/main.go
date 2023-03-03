483905e7b6090b4e08c55c402b780fa7c5503396
Jongseo Kim <80724255+ionc635@users.noreply.github.com>
2023-03-03 19:58:42 +0900

    [Steven] 6주차 문제 제출 (#43)
    * solve: 6주차_공통
    
    * solve: 6주차_개인

--- steven/main.go
+++ steven/main.go
@@ -21,4 +21,6 @@ func main() {
 	dp.GenerateParenthesis(3)
 	graph.FindJudge(3, [][]int{{1, 3}, {2, 3}})
 	graph.FindCenter([][]int{{1, 2}, {2, 3}, {4, 2}})
+	math.IsHappy(19)
+	sort.SearchInsert([]int{1, 2, 5, 7}, 2)
 }
