8f869c342dc660333bc0b4ff2bf0db9e4404a378
김예림 <kimyerim1935@naver.com>
2023-03-10 16:34:22 +0900

    solve: 8주차 개인

---
+++ Kimyerim1935/프로그래머스_120810.c
@@ -0,0 +1,15 @@
+/*
+ * Programers
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ * Problem Number: 120810
+ * Level: 0
+ * Algorithm: 코딩테스트 연습
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+   return num1 % num2;
+}
