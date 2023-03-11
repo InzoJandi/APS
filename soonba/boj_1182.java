e196dc75a3912e35a1c1d98fd77204a86c742f25
sunba <sunba2318@gmail.com>
2023-03-12 00:05:38 +0900

    solve: 8주차_공통_리팩토링

--- soonba/boj_1182.java
+++ soonba/boj_1182.java
@@ -12,10 +12,20 @@ import java.util.Arrays;
 
 /*
 수열을 배열로 초기화한다.
-모든 크기에 대해 모든 수열의 경우의 수를 dfs로 다 더하여 목표값과 같다면 count를 더해준다.
+'수를 더하거나' '안 더하거나' 의 경우의 수로 n에 대해 모든 부분집합 2^n 의 경우의수를 계산한다.
+5 0
+-7 -3 -2 5 8
+
+예제의 경우 아래와 같이 나눠진다.
+               0                    0
+        -7           0             -7
+   -10     -7     -3    0          -3
+ -12 -10 -9 -7  -5 -3  -2 0        -2
+ ...
  */
 
 public class Main {
+    static int T;
     static int target;
     static int total = 0;
 
@@ -24,27 +34,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] S = br.readLine().split(" ");
-        int T = Integer.parseInt(S[0]);
+        T = Integer.parseInt(S[0]);
         target = Integer.parseInt(S[1]);
         sequenceNum = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dfs(0,0);
 
-        for (int i = 1; i <= T; i++) {
-            dfs(0,i,0,0);
-        }
-
-        System.out.println(total);
+        int result = target == 0 ? total-1 : total;
+        System.out.println(result);
     }
 
-    static void dfs(int count, int sequenceSize, int temp, int nowIdx) {
-        if(count == sequenceSize) {
-            if(temp == target) {
-                total++;
-            }
+    static void dfs(int count, int temp) {
+        if(count == T) {
+            if(temp == target) total++;
             return;
         }
+        dfs(count+1, temp+sequenceNum[count]);
+        dfs(count+1, temp);
 
-        for (int i = nowIdx; i < sequenceNum.length; i++) {
-            dfs(count + 1, sequenceSize, temp + sequenceNum[i],i+1);
-        }
     }
 }
