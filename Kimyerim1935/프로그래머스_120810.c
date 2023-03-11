45dbaa82e1aa51d291738f9a8b0ffac62a18348d
김예림 <kimyerim1935@naver.com>
2023-03-11 22:03:35 +0900

    solve: 8주차 개인

--- Kimyerim1935/프로그래머스_120810.c
+++ Kimyerim1935/프로그래머스_120810.c
@@ -1,7 +1,7 @@
 /*
  * Programers
- * https://school.programmers.co.kr/learn/courses/30/lessons/12906
- * Problem Number: 120810
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=c
+ * Problem Number: 120808
  * Level: 0
  * Algorithm: 코딩테스트 연습
  */
@@ -10,6 +10,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
-   return num1 % num2;
+int* solution(int numer1, int denom1, int numer2, int denom2) {
+    int* answer = (int*)malloc(1);
+    
+    int a = denum1 * num2 + denum2 * num1;
+    int b = num1 + num2;
+    int c = 1;
+    
+    for(int i =1; i <= a; i++) {
+        if (a%i == 0 && b%i == 0) {
+            c = i;
+        }
+    }
+    
+    answer[0]= a/c;
+    answer[1]=b/c;
+    
+    return answer;
 }
